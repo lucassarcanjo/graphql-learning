376b49ac5b0856ad8bd1d3e5f465f863b1f6bb8d
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-03-01 20:02:42 -0300

    Add a greeting example

---
+++ server.js
@@ -0,0 +1,18 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello World",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await server.listen({ port: 9000 });
+
+console.log(`Server running at ${url}`);
